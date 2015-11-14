ba3e604e8bfdd6df72c5de9668db43a0d6d4e435
Jure Sorn <sornjure@gmail.com>
2015-11-14 19:07:48 +0100

    Initial commit

---
+++ update-site.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#
+# Usage: update-site.py 
+# Generates README.md and single image webpages, as defined in
+# photosrc.
+
+from __future__ import print_function
+import sys
+import re
+
+with open("template/template.html") as f:
+  TEMPLATE = f.readlines()
+
+def main():
+  with open("photosrc") as f:
+    lines = f.readlines()
+  username = [line.replace("USERNAME=", "").strip() for line in lines if "USERNAME=" in line][0]
+  readmeLink = "https://github.com/" + username + "/photos"
+  rootAddress = "http://" + username + ".github.io/photos/site/"
+  chain = [line.strip() for line in lines if not line.strip().startswith('#') and line.strip() and "USERNAME=" not in line]
+  generateReadme(chain, username)
+  generatePages(chain, readmeLink, rootAddress)
+
+def generateReadme(chain, username):
+  readme = "# photos\n\n"
+  for img in chain:
+    readme += getReadmeLine(img, username)
+  f = open("README.md", "w")
+  print(readme, file=f)
+
+def getReadmeLine(img, username):
+  return "[<img src=\"site/images/"+img+"\">](http://"+username+".github.io/photos/site/"+changeExtensionToHtml(img)+")\n\n"
+
+def generatePages(chain, readmeLink, rootAddress):
+  for i in xrange(0, len(chain)-1):
+    if i == 0:
+      back = readmeLink
+    else:
+      back = rootAddress + changeExtensionToHtml(chain[i-1])
+    if i == len(chain)-1:
+      forward = readmeLink
+    else:
+      forward = rootAddress + changeExtensionToHtml(chain[i+1])
+    page = ''.join(getPage(back, chain[i], forward))
+    filename = "site/" + changeExtensionToHtml(chain[i])
+    f = open(filename, "w")
+    print(page, file=f)
+
+def changeExtensionToHtml(string):
+  return re.sub("png", "html", re.sub("jpg", "html", string))
+
+def getPage(back, img, forward):
+  return [line.replace("BACK", back).replace("IMAGE", "images/"+img).replace("FORWARD", forward) for line in TEMPLATE]
+
+if __name__ == '__main__':
+  main()
